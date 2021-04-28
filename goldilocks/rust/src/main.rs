0d2812a12ea49f6dcb19c58585bb2c70de8e9d03
Jeffrey Rennie <surferjeff@gmail.com>
2021-04-27 18:00:29 -0700

    More sample rust.

--- goldilocks/rust/src/main.rs
+++ goldilocks/rust/src/main.rs
@@ -1,4 +1,7 @@
+use std::collections::{BTreeSet, HashSet};
+
 fn main() {
+    let url = Url::default();
     let mut url = Url {
         protocol: "http".into(),
         host_name: "www.google.com".into(),
@@ -6,10 +9,17 @@ fn main() {
         path: String::new()
     };
     let url2 = url.clone();
+    let url3 = url.clone();
+    let url4 = url.clone();
+    let mut sorted_urls = BTreeSet::new();
+    sorted_urls.insert(url3);
+    let mut urls = HashSet::new();
+    urls.insert(url4);
     println!("{:?}", url);
     url.incr_port();
     println!("{:?}", url);
     println!("url == url2? {}", url == url2);
+    println!("url > url2? {}", url > url2);
 }
 
 #[derive(Eq, PartialEq, Ord, PartialOrd, Clone, Hash, Default, Debug)]
