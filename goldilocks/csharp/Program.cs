acbca5373cfb586a48f01c31cab50c2af02a6fe5
Jeffrey Rennie <surferjeff@gmail.com>
2021-05-05 20:08:08 -0700

    csharp sample

---
+++ goldilocks/csharp/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace goldilocks
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!\n{0:C}", "Jeff", 123);
+        }
+    }
+}
