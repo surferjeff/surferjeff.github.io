2468dfa91289aebaf58b1f91cc8be973ac051a0c
Jeffrey Rennie <surferjeff@gmail.com>
2021-05-07 09:42:36 -0700

    Complete tests pass.

--- tryfinally/src/main.rs
+++ tryfinally/src/main.rs
@@ -28,10 +28,21 @@ pub mod tests {
         let mut f = tempfile::tempfile().unwrap();
         f.write_all(b"Hello World").unwrap();
         f.seek(SeekFrom::Start(0)).unwrap();
+
+        // Peek the first 5 bytes.
         let mut buf = [0u8; 5];
         peek_file(&mut f, &mut buf).unwrap();
         assert_eq!(b"Hello", &buf);
+
         // The previous call to peek_file should not have advanced the file pointer.
+        peek_file(&mut f, &mut buf).unwrap();
+        assert_eq!(b"Hello", &buf);
+
+        // Try an read that's too big and will fail.
+        let mut big_buf = [0u8; 100];
+        assert!(peek_file(&mut f, &mut big_buf).is_err());
+
+        // But the file pointer should still be at the beginning of the file.
         peek_file(&mut f, &mut buf).unwrap();
         assert_eq!(b"Hello", &buf);
     }
