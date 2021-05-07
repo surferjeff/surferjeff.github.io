ad1185990cd28085a80113314cdf12deeeb14867
Jeffrey Rennie <surferjeff@gmail.com>
2021-05-07 09:52:29 -0700

    macro with no body

--- tryfinally/src/main.rs
+++ tryfinally/src/main.rs
@@ -2,12 +2,24 @@ use std::io::{self, Read, Seek, SeekFrom};
 use std::fs::File;
 
 
-fn main() {
-    println!("Hello, world!");
+macro_rules! tryf {
+    ($tblock:block finally $fblock:block) => {
+        
+    };
 }
+
+fn main() {
+    tryf!{{
+        println!("hello");
+    } finally {
+        println!("world");
+    }};
+}
+
 
 fn peek_file(f: &mut File, buffer: &mut [u8]) -> io::Result<()>
 {
+
     let offset = f.seek(SeekFrom::Current(0))?;
     let mut __try = || -> Result<_, _> {
         f.read_exact(buffer)
